2b3bdd752c6e20aaf5aeeb719d857cfdcffc9586
blaine <mw1984@gmx.de>
2018-01-31 16:45:49 +0100

    Several changes

--- tmux_session.zsh
+++ tmux_session.zsh
@@ -1,18 +1,18 @@
 #!/bin/zsh
 
+# Session name
 SESSION=*
-tmux="tmux"
 
-# if the session is already running, just attach to it.
-$tmux has-session -t $SESSION
+# If the session is already running, just attach to it.
+tmux has-session -t $SESSION
 if [ $? = 0 ]; then
        echo "Session $SESSION already exists. Attaching."
-       sleep 1
-       $tmux attach -t $SESSION
+       sleep 2
+       tmux attach -t $SESSION
        exit 0;
 fi
 
-# create a new session, named $SESSION, and detach from it
-$tmux new-session -d -s $SESSION
-$tmux select-window -t $SESSION:0
-$tmux attach -t $SESSION
+# Create a new session, named $SESSION, and detach from it
+tmux new-session -d -s $SESSION
+tmux select-window -t $SESSION:0
+tmux attach -t $SESSION
