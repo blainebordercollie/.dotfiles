3d4b78dea4f7de93b0a652c2b55d9e3ce3df83ad
blaine <mw1984@gmx.de>
2018-02-03 01:18:47 +0100

    Add clean info stream for user

--- tmux_session.zsh
+++ tmux_session.zsh
@@ -3,16 +3,20 @@
 # Session name
 SESSION=*
 
-# If the session is already running, just attach to it.
-tmux has-session -t $SESSION
+# If the session is already running, just attach to it. stderr > null
+tmux has-session -t $SESSION 2> /dev/null
 if [ $? = 0 ]; then
-       echo "Session $SESSION already exists. Attaching."
-       sleep 2
+       echo "Session $SESSION already exists. Attaching..."
+       sleep 1
        tmux attach -t $SESSION
        exit 0;
 fi
 
+# Echo whats going on
+echo "No active sessions found."
+echo "Creating new session named: $SESSION ..."
+sleep 1
 # Create a new session, named $SESSION, and detach from it
 tmux new-session -d -s $SESSION
-tmux select-window -t $SESSION:0
+# tmux select-window -t $SESSION:0
 tmux attach -t $SESSION
